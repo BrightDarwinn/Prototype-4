f4d8509220abea4c2f1cdd5f7e90be0cab67c2a8
BrightDarwinn <164432@student.vansd.org>
2023-04-10 14:01:32 -0700

    fghjkl

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerUp = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,22 @@ public class PlayerController : MonoBehaviour
     {
      float forwardInput = Input.GetAxis("Vertical");
      playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);   
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Collided with:" + collision.gameObject.name + "with power up set to" + hasPowerUp);
+        }
     }
 }
